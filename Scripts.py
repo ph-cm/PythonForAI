1c1e80edfd28520081c5c5a70a7eb87ac138c114
Pedro Henrique <trabalho.prog.pedro@gmail.com>
2025-02-21 18:34:05 -0300

    Scripts and Some DataType Notation

---
+++ Scripts.py
@@ -0,0 +1,22 @@
+# Writing programs and running them
+
+# Script = text file with .py extension
+# Its commun to use IDEto create scripts like the VsCode used in this reminder
+
+a = 20
+b = 12
+
+sum = a + b
+sub = a - b
+mul = a * b
+div = a / b
+
+print("So here's the example of an script about some basic math")
+print(sum)
+print(type(sub))
+print(type(div))
+print(mul)
+
+# note that because python is a language with low level of datatype(you dont need to declare the type of a variable)
+# when i print the type of sub and the type of div, both has diferent types automatically, so the language does that
+# (it has some problems about it but that can be talked after)
