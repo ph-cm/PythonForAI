3d04e5fcd207c300bb0237231a541deef06b058b
Pedro Henrique <trabalho.prog.pedro@gmail.com>
2025-02-21 18:24:05 -0300

    Variables

---
+++ Variables.py
@@ -0,0 +1,11 @@
+# A stored of data
+
+user_name = "Pedro"
+print(user_name)
+
+user_age = 19
+user_interests = ["Sports", "Music", "Games"]
+
+print(f"{user_name} is {user_age} years old and has interests in {user_interests}.")
+
+#So besides all of that, its pretty simple the firsts concepts so does it about variables
